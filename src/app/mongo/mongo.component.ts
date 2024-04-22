f4613d2f77791d7aa0b5d7ea11e496f69d15fa2d
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-22 09:06:23 +0530

    mongo

---
+++ src/app/mongo/mongo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mongo',
+  standalone: true,
+  imports: [],
+  templateUrl: './mongo.component.html',
+  styleUrl: './mongo.component.css'
+})
+export class MongoComponent {
+
+}
